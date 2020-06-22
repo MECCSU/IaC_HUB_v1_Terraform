aaec14dd14a1f4c2078785f8fe6a21e671c5edd9
mou.idri@gmail.com <mou.idri@gmail.com>
2020-06-22 17:23:08 +0200

    commit with terraform files first update

--- provider.tf
+++ provider.tf
@@ -1,10 +1,7 @@
 provider "azurerm" {
   version="2.0.0"
   features {}
-  #subscription_id = "${var.azure-subscription-id}"
-  #client_id       = "${var.azure-client-app-id}"
-  #client_secret   = "${var.azure-client-secret-password}"
-  #tenant_id       = "${var.azure-tenant-id}"
+
 }
 
 terraform {
